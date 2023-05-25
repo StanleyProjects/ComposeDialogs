3f4aae8818f0839075610a99a1daedcd285d523f
Stanley Wintergreen <kepocnhh@gmail.com>
2023-05-25 18:55:19 +0300

    variant extensions

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,3 +1,20 @@
+import com.android.build.gradle.api.BaseVariant
+import sp.gx.core.GitHub
+import sp.gx.core.Maven
+import sp.gx.core.kebabCase
+
+version = "0.0.1"
+
+val maven = Maven.Artifact(
+    group = "com.github.kepocnhh",
+    id = rootProject.name,
+)
+
+val gh = GitHub.Repository(
+    owner = "StanleyProjects",
+    name = rootProject.name,
+)
+
 repositories {
     google()
     mavenCentral()
@@ -6,6 +23,20 @@ repositories {
 plugins {
     id("com.android.library")
     id("kotlin-android")
+}
+
+fun BaseVariant.getVersion(): String {
+    check(flavorName.isEmpty())
+    return when (buildType.name) {
+        "debug" -> kebabCase(version.toString(), "SNAPSHOT")
+        "release" -> version.toString()
+        else -> error("Build type \"${buildType.name}\" is not supported!")
+    }
+}
+
+fun BaseVariant.getOutputFileName(extension: String): String {
+    require(extension.isNotEmpty())
+    return "${kebabCase(rootProject.name, getVersion())}.$extension"
 }
 
 android {
