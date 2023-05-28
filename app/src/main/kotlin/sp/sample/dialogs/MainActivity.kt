89a2a3f08445e21e1fa8eb79425094dafd6b1a9a
Stanley Wintergreen <kepocnhh@gmail.com>
2023-05-28 14:06:26 +0300

    dialog style

--- app/src/main/kotlin/sp/sample/dialogs/MainActivity.kt
+++ app/src/main/kotlin/sp/sample/dialogs/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,11 +21,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import sp.ax.jc.dialogs.Dialog
 
 internal class MainActivity : AppCompatActivity() {
@@ -57,11 +62,60 @@ internal class MainActivity : AppCompatActivity() {
                     )
                     if (dialog) {
                         Dialog(
-                            modifier = Modifier.background(Color.White),
+                            modifier = Modifier
+                                .defaultMinSize(280.dp)
+                                .background(
+                                    color = Color.White,
+                                    shape = RoundedCornerShape(28.dp),
+                                )
+                                .padding(24.dp),
                             onDismissRequest = {
                                 dialog = false
                             },
-                            message = "dialog with message",
+                            title = Dialog.Text(
+                                modifier = Modifier
+                                    .padding(bottom = 16.dp),
+                                value = "title",
+                                style = TextStyle(fontSize = 24.sp),
+                            ),
+                            message = Dialog.Text(
+                                modifier = Modifier
+                                    .padding(bottom = 24.dp),
+                                value = "message",
+                                style = TextStyle(fontSize = 14.sp),
+                            ),
+                            buttons = listOf(
+                                Dialog.Text(
+                                    modifier = Modifier
+                                        .clip(RoundedCornerShape(20.dp))
+                                        .height(40.dp)
+                                        .clickable { dialog = false }
+                                        .padding(start = 12.dp, end = 12.dp)
+                                        .wrapContentHeight(),
+                                    value = "foo",
+                                    style = TextStyle(fontSize = 14.sp),
+                                ),
+                                Dialog.Text(
+                                    modifier = Modifier
+                                        .clip(RoundedCornerShape(20.dp))
+                                        .height(40.dp)
+                                        .clickable { dialog = false }
+                                        .padding(start = 12.dp, end = 12.dp)
+                                        .wrapContentHeight(),
+                                    value = "cancel",
+                                    style = TextStyle(fontSize = 14.sp),
+                                ),
+                                Dialog.Text(
+                                    modifier = Modifier
+                                        .clip(RoundedCornerShape(20.dp))
+                                        .height(40.dp)
+                                        .clickable { dialog = false }
+                                        .padding(start = 12.dp, end = 12.dp)
+                                        .wrapContentHeight(),
+                                    value = "ok",
+                                    style = TextStyle(fontSize = 14.sp),
+                                ),
+                            ),
                         )
                     }
                 }
--- lib/src/main/kotlin/sp/ax/jc/dialogs/Dialog.kt
+++ lib/src/main/kotlin/sp/ax/jc/dialogs/Dialog.kt
@@ -1,20 +1,70 @@
 package sp.ax.jc.dialogs
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+
+object Dialog {
+    data class Text(
+        val modifier: Modifier,
+        val value: String,
+        val style: TextStyle,
+    )
+}
 
 @Composable
 fun Dialog(
     modifier: Modifier,
     onDismissRequest: () -> Unit,
-    message: String,
+    properties: DialogProperties = DialogProperties(),
+    title: Dialog.Text,
+    message: Dialog.Text,
+    buttons: List<Dialog.Text>,
 ) {
-    Dialog(onDismissRequest = onDismissRequest) {
-        Box(modifier) {
-            BasicText(text = message)
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = properties,
+    ) {
+        Column(modifier = modifier) {
+            check(title.value.isNotEmpty())
+            BasicText(
+                modifier = title.modifier,
+                text = title.value,
+                style = title.style,
+            )
+            check(message.value.isNotEmpty())
+            BasicText(
+                modifier = message.modifier,
+                text = message.value,
+                style = message.style,
+            )
+            Row(modifier = Modifier.align(Alignment.End)) {
+                checkNotNull(buttons.firstOrNull()).also { text ->
+                    BasicText(
+                        modifier = text.modifier,
+                        text = text.value,
+                        style = text.style,
+                    )
+                }
+                for (i in 1 until buttons.size) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                    val text = buttons[i]
+                    BasicText(
+                        modifier = text.modifier,
+                        text = text.value,
+                        style = text.style,
+                    )
+                }
+            }
         }
     }
 }
