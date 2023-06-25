55d8877f31e1e976a64516f39c83695f45d25cd5
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-25 16:12:51 +0300

    test colors

--- lib/src/test/kotlin/sp/ax/jc/dialogs/DialogColorsTest.kt
+++ lib/src/test/kotlin/sp/ax/jc/dialogs/DialogColorsTest.kt
@@ -1,5 +1,7 @@
 package sp.ax.jc.dialogs
 
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.filterToOne
@@ -53,6 +55,104 @@ internal class DialogColorsTest {
                         .assertExists()
                         .assertIsDisplayed()
                         .assert(hasTextColor(checkNotNull(dialogStyle).foreground))
+                }
+            }
+    }
+
+    @Test
+    fun colorsCustomTest() {
+        val message = "message"
+        val button = "button"
+        val buttons = setOf("foo", "bar", "baz")
+        check(buttons.size > 1)
+        check(!buttons.contains(button))
+        var dialogStyle: DialogStyle? = null
+        val foregroundColor: Color = Color.Green
+        val backgroundColor: Color = Color.Red
+        rule.setContent {
+            dialogStyle = LocalDialogStyle.current
+            Dialog(
+                button = button to {},
+                buttons = buttons.map { it to {} }.toTypedArray(),
+                onDismissRequest = {},
+                message = message,
+                foregroundColor = foregroundColor,
+                backgroundColor = backgroundColor,
+            )
+        }
+        check(checkNotNull(dialogStyle).foreground != foregroundColor)
+        check(checkNotNull(dialogStyle).background != backgroundColor)
+        // todo background
+        rule.onNode(isDialog())
+            .assertExists()
+            .assertIsDisplayed()
+            .onChildren().also { children ->
+                children.filterToOne(hasText(message))
+                    .assertExists()
+                    .assertIsDisplayed()
+                    .assert(hasTextColor(foregroundColor))
+                children.filterToOne(hasText(button))
+                    .assertExists()
+                    .assertIsDisplayed()
+                    .assert(hasTextColor(foregroundColor))
+                buttons.forEach {
+                    children.filterToOne(hasText(it))
+                        .assertExists()
+                        .assertIsDisplayed()
+                        .assert(hasTextColor(foregroundColor))
+                }
+            }
+    }
+
+    @Test
+    fun colorsLocalTest() {
+        val message = "message"
+        val button = "button"
+        val buttons = setOf("foo", "bar", "baz")
+        check(buttons.size > 1)
+        check(!buttons.contains(button))
+        var dialogStyle: DialogStyle? = null
+        val foregroundColor: Color = Color.Green
+        val backgroundColor: Color = Color.Red
+        rule.setContent {
+            dialogStyle = LocalDialogStyle.current
+            check(checkNotNull(dialogStyle).foreground != foregroundColor)
+            check(checkNotNull(dialogStyle).background != backgroundColor)
+            CompositionLocalProvider(
+                LocalDialogStyle provides DialogStyle(
+                    background = backgroundColor,
+                    foreground = foregroundColor,
+                )
+            ) {
+                dialogStyle = LocalDialogStyle.current
+                Dialog(
+                    button = button to {},
+                    buttons = buttons.map { it to {} }.toTypedArray(),
+                    onDismissRequest = {},
+                    message = message,
+                )
+            }
+        }
+        check(checkNotNull(dialogStyle).foreground == foregroundColor)
+        check(checkNotNull(dialogStyle).background == backgroundColor)
+        // todo background
+        rule.onNode(isDialog())
+            .assertExists()
+            .assertIsDisplayed()
+            .onChildren().also { children ->
+                children.filterToOne(hasText(message))
+                    .assertExists()
+                    .assertIsDisplayed()
+                    .assert(hasTextColor(foregroundColor))
+                children.filterToOne(hasText(button))
+                    .assertExists()
+                    .assertIsDisplayed()
+                    .assert(hasTextColor(foregroundColor))
+                buttons.forEach {
+                    children.filterToOne(hasText(it))
+                        .assertExists()
+                        .assertIsDisplayed()
+                        .assert(hasTextColor(foregroundColor))
                 }
             }
     }
